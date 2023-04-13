0dcb44efcbe07a6b67459c5c469e73fe34b03905
XtremeMC <yassineouertani877@gmail.com>
2023-04-13 20:55:01 +0100

    updated

--- draw.py
+++ draw.py
@@ -3,15 +3,14 @@ from main import *
 class shapes:
     """shows a shape in a graphical window with number of sides and size specified by user."""
     def __init__(self, master, sides, size):
-        self.master = master
         t.clear()
-        self.hideturtle()
-        self.pendown()
-        self.speed(2)
-        self.goto(0, 0)
+        master.hideturtle()
+        master.pendown()
+        master.speed(2)
+        master.goto(0, 0)
         for _ in range(sides):
-            self.fd(size)
-            self.lt(360/(sides))
+            master.fd(size)
+            master.lt(360/(sides))
 
 icon()
 t.title(f"draw for {user}")
