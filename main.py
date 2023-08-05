d72dd84e920d7376beb41f76f5be85d34abb9204
xtrememc <yassineouertani877@gmail.com>
2023-08-05 14:39:02 +0100

    updated

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ Made by Xtreme MC Studios 2023. © All rights reserved.
 
 import getpass, time
 
+# getting the username
 try:
     user = getpass.getuser()
 except:
     user = "guest"
 
+# getting local date and time
 datetime = time.strftime("%H:%M %d/%m/%Y")
 
+# welcoming the user
 print(f"welcome, {user}.")
 print(datetime)
 
+# setting up the icon of the GUI
 def icon(root): root.iconbitmap("assets\logo-pyOS.ico")
--- password.py
+++ password.py
@@ -1,49 +1,41 @@
 from main import *
 from tkinter import messagebox
 import tkinter as tk
-import random, string
+import string
 
-def generate():
-    def new():
-        char = string.ascii_letters + string.digits + string.punctuation
-        password = "".join(random.choices(char, k=random.randint(8,16)))
-        showPw = messagebox.showinfo(f"Password for {user}", f"Here is your password:\n\n{password}")
-
-    generatePw = tk.Button(root, text="generate", command=new)
-    generatePw.pack(padx=5, pady=5)
-
-def upgrade():
-    letters = string.ascii_letters
-    num = string.digits
-    punc = string.punctuation
-
-    userInput = tk.Entry(root)
-    userInput.pack(padx=5, pady=5)
-
-    def check():
-        password = userInput.get()
-        if not any(word in password for word in letters):
-            noLetter = messagebox.showinfo("Error", "Your password should contain at least one letter.")
-        elif not any(word in password for word in num):
-            noNum = messagebox.showinfo("Error", "Your password should contain at least one number.")
-        elif not any(word in password for word in punc):
-            noPunc = messagebox.showinfo("Error", "Your password should contain at least one special character.")
-        elif len(password) < 8:
-            tinyLen = messagebox.showinfo("Error", "Your password should contain at least 8 characters.")
-        else:
-            successMsg = messagebox.showinfo("Congratulations", "Your password is okay!")
-
-    upg = tk.Button(root, text="upgrade", command=check)
-    upg.pack(padx=5, pady=5)
+# setting up characters
+letters = string.ascii_letters
+num = string.digits
+punc = string.punctuation
 
 root = tk.Tk()
 root.title(f"password manager for {user}")
-root.geometry("200x100")
-
+root.geometry("400x200")
 icon(root)
 
-generate()
-upgrade()
+# showing a paragraph
+paragraph = tk.Label(root, text="Type your password below to upgrade.\n\nNote: We do not collect any data.")
+paragraph.pack(padx=5, pady=5)
 
-tk.mainloop()
+userInput = tk.Entry(root, width=50)
+userInput.pack(padx=5, pady=5)
+
+# checking the password
+def check():
+    password = userInput.get()
+    if not any(word in password for word in letters):
+        noLetter = messagebox.showinfo("Error", "Your password should contain at least one letter.")
+    elif not any(word in password for word in num):
+        noNum = messagebox.showinfo("Error", "Your password should contain at least one number.")
+    elif not any(word in password for word in punc):
+        noPunc = messagebox.showinfo("Error", "Your password should contain at least one special character.")
+    elif len(password) < 8:
+        tinyLen = messagebox.showinfo("Error", "Your password should contain at least 8 characters.")
+    else:
+        successMsg = messagebox.showinfo("Congratulations", "Your password is okay!")
+
+upg = tk.Button(root, text="upgrade", command=check, width=10)
+upg.pack(padx=5, pady=5)
+
+root.mainloop()
 exit()
